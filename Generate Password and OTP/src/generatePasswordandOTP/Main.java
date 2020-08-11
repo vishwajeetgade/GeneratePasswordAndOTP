7931fd19aeb4fdf82c7e4a777c1c318b14abf9d3
vishwajeetgade <53171281+vishwajeetgade@users.noreply.github.com>
2020-08-11 22:45:36 +0530

    Add files via upload
    no Reapeated

--- Generate Password and OTP/src/generatePasswordandOTP/Main.java
+++ Generate Password and OTP/src/generatePasswordandOTP/Main.java
@@ -5,34 +5,41 @@ public class Main {
 
 	public static void main(String[] args) {
 		int len = 0;
-		
-		System.out.println(otp());
-		
-		
+		Set<String> check = new HashSet<>();
+		String result = otp();
+		while(true) {
+			if(!check.contains(result)) {
+				check.add(result);
+				System.out.println(result);
+				break;
+			}else {
+				result = otp();
+			} 
+		}	
 	}
 	
-	public static char[] password(int len) {
+	public static String password(int len) {
 		String allCaps ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 		String lowecase ="abcdefghijklmnopqrstuvwxyz";
 		String num ="0123456789";
 		String cha = "@#$&";
 		String allPas = allCaps+lowecase+num+cha;
-		char[] ch = new char[len];
+		String str = "";
 		Random rnd = new Random();
 		for(int i=0; i<len; i++) {
-			ch[i] = allPas.charAt(rnd.nextInt(allPas.length()));
+			str += Character.toString(allPas.charAt(rnd.nextInt(allPas.length())));
 		}
-		return ch;
+		return str;
 	}
 	
-	public static char[] otp() {
+	public static String otp() {
 		String num ="0123456789";
-		char[] ch = new char[6];
 		Random rnd = new Random();
+		String str ="";
 		for(int i=0; i<6; i++) {
-			ch[i] = num.charAt(rnd.nextInt(10));
+			str += Character.toString(num.charAt(rnd.nextInt(10)));
 		}
-		return ch;
+		return str;
 	}
 
 }
